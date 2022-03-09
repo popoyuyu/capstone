be009d1bdf3fc2bb8083a0310704bd217dedb185
Jeff Lai <pjefflai@gmail.com>
2022-03-09 06:26:22 -0800

    inital auth

--- basketball-app/src/App.js
+++ basketball-app/src/App.js
@@ -3,6 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <h3> Register User</h3>
+        <input placeholder='Email...' />
+        <input placeholder='Password...' />
+
+        <button>Create User</button>
+      </div>
+
+      <div>
+        <h3>Login</h3>
+        <input placeholder='Email...' />
+        <input placeholder='Password...' />
+
+        <button>Login</button>
+      </div>
+
+      <h4>User Logged In: </h4>
+
+      <button> Sign Out</button>
     </div>
   );
 }
